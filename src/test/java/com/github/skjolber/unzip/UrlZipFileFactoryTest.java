8f38d393ce586a3a9986ac544e9f188d2cc36fa7
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2024-10-01 20:39:28 +0200

    Adjust test

--- src/test/java/com/github/skjolber/unzip/UrlZipFileFactoryTest.java
+++ src/test/java/com/github/skjolber/unzip/UrlZipFileFactoryTest.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UrlZipFileFactoryTest {
 
